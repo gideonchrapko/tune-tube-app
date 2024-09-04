345be4b3e05e589bbb983e1312250d7bad33ad3a
gideonchrapko <gideonchrapko@hotmail.ca>
2024-09-04 11:49:06 -0400

    proper private key

--- config/firebase.ts
+++ config/firebase.ts
@@ -4,12 +4,12 @@ import { initFirestore } from "@auth/firebase-adapter";
 import { cert } from "firebase-admin/app";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET!,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID!,
+  appId: process.env.NEXT_PUBLIC_APP_ID!,
 };
 
 const app = initializeApp(firebaseConfig);
@@ -19,7 +19,7 @@ export const firestore = initFirestore({
   credential: cert({
     projectId: process.env.AUTH_FIREBASE_PROJECT_ID!,
     clientEmail: process.env.AUTH_FIREBASE_CLIENT_EMAIL!,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(
+    privateKey: process.env.AUTH_FIREBASE_PRIVATE_KEY?.replace(
       /\\n/g,
       "\n",
     ) as string,
