6b2255899ac45cad974506431172e8a0d611857e
gideonchrapko <gideonchrapko@hotmail.ca>
2024-10-24 13:33:08 -0400

    pushing up

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,9 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
 
-type ResponseData = {
-  message: string;
-};
-
-export default function handler(res: NextApiResponse<ResponseData>) {
-  res.status(200).json({ message: "Hello from TypeScript API!" });
+export async function GET() {
+  return NextResponse.json({ message: "Hello from TypeScript API!" });
 }
